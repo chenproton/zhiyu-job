f5f4f9fde7370fa2eef386883cf5b25394567af1
Developer <dev@zhiyu.com>
2026-06-24 17:31:45 +0800

    Revert "feat: 新增企业合作伙伴详情页 /admin/brands/partner/[id]"
    This reverts commit 7dfd00ef05824e6fae806d746a9215460abb51cf.

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PlatformShellWrapper } from '@/components/shared/platform-shell-wrapper'
-
-export default function AdminRootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <PlatformShellWrapper>{children}</PlatformShellWrapper>
-}
